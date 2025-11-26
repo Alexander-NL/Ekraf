77f55b96cf989bbb2bc75bcafbcb41d040295440
Alexander Nathanael Lubis <alexnathan168@gmail.com>
2025-11-27 02:13:29 +0700

    Asset change
    chamber 3-4 and boss done reskinned
    - fixed respawn issue
    - added show best score now

--- Assets/Script/Boss/ShowDeath.cs
+++ Assets/Script/Boss/ShowDeath.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 public class ShowDeath : MonoBehaviour
 {
     public TextMeshProUGUI Deathtext;
+    public TextMeshProUGUI BestScore;
 
     public void UpdateText()
     {
         Deathtext.text = "HERO'S TOTAL DEATH: " + SaveSystem.Instance.Death;
+        int bestScore = SaveSystem.Instance.GetDeathTotal();
+        BestScore.text = "FEWEST DEATH: " + bestScore;
     }
 }
